c041cad81262515628804ba9d8b3e4683a7dc6ff
Karol Walesiak <86485219+DeltaWave97@users.noreply.github.com>
2022-11-17 06:07:50 +0100

    Przygotowanie do robienia zadań dalej

---
+++ Zadania/TypyDanych4/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Do poniższych zadań nie możesz korzystać z żadnej wbudowanej metody 
+która by rozwiązywała zadanie (np. Contains, Min, Max). Wszystkie 
+zadania należy napisać samemu sprawdzając wartość po wartości w 
+tablicy. 
+Do generowania tablicy napisz metodę, która będzie ją zwracać z 
+wylosowanymi wartościami z podanego przedziału. 
+ 
+UWAGA – jeśli jest to możliwe, to do każdego zadania napisz metodę 
+obsługującą liczby całkowite oraz drugą metodę obsługującą liczby 
+zmiennoprzecinkowe (np. double) 
+ 
+1.  Napisz metodę, która dla podanej tablicy zwróci największą liczbę. 
+2.  Napisz metodę, która dla podanej tablicy zwróci drugą największą liczbę. 
+3.  Napisz metodę, która dla podanej tablicy zwróci średnią, medianę oraz 
+modę. 
+4.  Napisz metodę, która dla podanej tablicy oraz podanej wartości zwróci 
+informację, ile razy dana liczba występuję w owej tablicy (czyli metoda 
+przyjmuje dwa argumenty: tablice oraz liczbę. następnie sprawdza ile 
+razy ta liczba wystąpiła w tej tablicy i zwraca tą ilość). 
+5.  Napisz metodę, która dla podanej tablicy oraz podanej wartości zwróci 
+informację, czy dana wartość występuje w tablicy (czyli metoda 
+przyjmuje dwa argumenty: tablice oraz liczbę. następnie sprawdza czy 
+istnieje w tablicy dana liczba, a potem zwraca true/false). 
+6.  Napisz metodę, która dla podanych dwóch tablic zwróci jedną tablicę 
+będącą ich połączeniem. 
+7.  Napisz metodę, która dla podanej tablicy zwróci nową tablicę z 
+odwróconą kolejnością wartości (np. tablica A ma wartości: 1,2,3,4,5. 
+Tablica B czyli wynik metody: 5,4,3,2,1). 
+8.  Napisz metodę, która dla podanej tablicy zwróci nową tablicę, ale bez 
+powtórzeń wartości (np. tablica A ma wartości: 1,1,2,2,3. Tablica B czyli 
+wynik metody: 1,2,3). 
+9.  Napisz metodę, która pozwoli do przekazanej tablicy dodawać nową 
+wartość na jej początku. 
+10. Napisz metodę, która przyjmuje trzy argumenty: tablicę, nową wartość, 
+indeks w którym należy dodać nową wartość. Np. mamy tablicę 
+[0,1,2,3,4,5], metoda AddOnIndex(tablica, nowaWartosc, 
+indeksNowejWartosci) dla przekazanych wartości AddOnIndex(tablica, 
+101, 2), zwróci wynik: [0,1,101,2,3,4,5]. 
+11. Przeczytaj o algorytmach sortowania na stronie  https://code-
+maze.com/sorting-algorithms-csharp/ i następnie dla 5 wybranych 
+algorytmów napisz metodę, która dla podanej tablicy zwróci nową 
+tablicę, ale z posortowanymi wartościami od największej do 
+najmniejszej. 
+ */
